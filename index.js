286ff8ecc20da0bd0bbcbec65bf70d74dc43d98c
David Tenenbaum <github email address>
2018-06-27 20:00:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  const argArr = [];
+  args.forEach(function (arg) {
+    argArr.push(arg);
+  })
+  return fn.apply(thisValue, argArr);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+}
